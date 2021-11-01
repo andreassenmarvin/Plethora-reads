393cf977f3beb23ab82e0f8310c3aa0d039fcb39
andreassenmarvin <marvinandreassen@gmail.com>
2021-11-01 19:08:58 +0300

    Add api networks

---
+++ app/src/main/java/com/example/plethorareads/ApiService.java
@@ -0,0 +1,12 @@
+package com.example.plethorareads;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiService {
+    @GET("volumes")
+    Call<BookResponse> getMyJSON(
+            @Query("q") String query,
+            @Query("maxResults") int maxResults);
+}
