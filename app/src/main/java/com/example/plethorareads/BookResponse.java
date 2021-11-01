885329a93c8767ca435dfbe890c81aa7cb80fda3
andreassenmarvin <marvinandreassen@gmail.com>
2021-11-01 19:10:57 +0300

    Add models classes

---
+++ app/src/main/java/com/example/plethorareads/BookResponse.java
@@ -0,0 +1,18 @@
+package com.example.plethorareads;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class BookResponse implements Serializable {
+
+    @SerializedName("items")
+    @Expose
+    private List<Item> items = null;
+
+    public List<Item> getItems() {
+        return items;
+    }
+}
---
+++ app/src/main/java/com/example/plethorareads/VolumeInfo.java
@@ -0,0 +1,46 @@
+package com.example.plethorareads;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class VolumeInfo implements Serializable {
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("authors")
+    @Expose
+    private List<String> authors = null;
+    @SerializedName("publisher")
+    @Expose
+    private String publisher;
+    @SerializedName("publishedDate")
+    @Expose
+    private String publishedDate;
+    @SerializedName("description")
+    @Expose
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
