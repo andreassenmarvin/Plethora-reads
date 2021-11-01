100fc349b19e7b1e5e4f94721df64ecdc8420cce
andreassenmarvin <marvinandreassen@gmail.com>
2021-11-01 19:13:02 +0300

    Create main activity

--- app/src/main/java/com/example/plethorareads/MainActivity.java
+++ app/src/main/java/com/example/plethorareads/MainActivity.java
@@ -1,14 +1,100 @@
 package com.example.plethorareads;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MAX_RESULTS = 5;
+
+    private List<Item> volumeInfoList;
+    private ProgressDialog dialog;
+    private ApiService api;
+    private BooksAdapter adapter;
+    private RecyclerView recyclerView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        api = RetroClient.getApiService();
+
+        dialog = new ProgressDialog(MainActivity.this);
+        dialog.setMessage("Loading...");
+
+        recyclerView = findViewById(R.id.recycler_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        adapter = new BooksAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void doSearch(final String query) {
+        dialog.show();
+
+        Call<BookResponse> call = api.getMyJSON(query, MAX_RESULTS);
+        call.enqueue(new Callback<BookResponse>() {
+            @Override
+            public void onResponse(Call<BookResponse> call, Response<BookResponse> response) {
+                dialog.dismiss();
+
+                if (response.isSuccessful()) {
+                    volumeInfoList = response.body().getItems();
+                    recyclerView.smoothScrollToPosition(0);
+                    adapter.setVolumeInfo(volumeInfoList);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<BookResponse> call, Throwable t) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doSearch(query);
+        }
     }
 }
